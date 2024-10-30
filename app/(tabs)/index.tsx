415bdd3ac484d6742f09f768f8d17a5c171b7219
tejasveedighe <tejasveedighe@gmail.com>
2024-10-30 11:34:05 +0530

    feat(app): Create image component to render images
    Update the layout, added tabs to the project, add expo-image package

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,25 @@
+import ImageViewer from "@/components/ImageViewerComponent";
+import { StyleSheet, View } from "react-native";
+
+const PlaceholderImage = require("@/assets/images/background-image.png");
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+  },
+  imageContainer: {
+    flex: 1,
+  },
+});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Home" }}/>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="about" options={{ title: "About" }} />
     </Stack>
   );
